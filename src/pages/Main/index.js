0906172b74c64d24c3b04ec237bc61b5fd3fb30c
John Victor <johnvictorio3@gmail.com>
2019-10-25 16:43:36 -0300

    [update] styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,30 +1,22 @@
 import React from 'react';
 
-import {Text, Image, ImageBackground, StatusBar} from 'react-native';
+import {Text, View, StatusBar, TouchableOpacity} from 'react-native';
 
 import styles from './styles';
 
 const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover">
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    {/* <Image
-      source={{
-        uri:
-          'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
-      }}
-      style={styles.logo}
-      resizeMode="contain"
-    /> */}
-    <Text style={styles.welcome}>PharmRD</Text>
-    <Text style={[styles.instructions, styles.fileName]}>
-      Receituário Digital
-    </Text>
-  </ImageBackground>
+  <View style={styles.container}>
+    <StatusBar barStyle="light-content" backgroundColor="orange" />
+    <View style={styles.content}>
+      <Text style={styles.welcome}>PharmRD</Text>
+      <Text style={[styles.instructions, styles.fileName]}>
+        Receituário Digital
+      </Text>
+    </View>
+    <TouchableOpacity>
+      <Text style={styles.instructions}>Ler QRCode</Text>
+    </TouchableOpacity>
+  </View>
 );
 
 export default Main;
